9e6d2e714997e8315bd9e90a767f77d68345ff0c
Alexander-Mitrofanov <mitrofan@informatik.uni-freiburg.de>
2022-07-06 18:01:38 +0200

    tasks + implementation

--- README.md
+++ README.md
@@ -7,3 +7,119 @@ Lehrstuhl für Bioinformatik - Institut für Informatik - *http://www.bioinf.uni
 ###### SS 2021/2022
 ##### Exercise sheet 10: Introduction to mapping
 ---
+
+### _Exercise 4 - Programming Assignment_
+In this exersice you will implement Burrows-Wheeler Transform (BWT) and its inverse.
+
+**a)** In order to start building the Burrows-Wheeler matrix you need to implement a helper function which return all the rotations of the given string.
+Implement the function `rotations` which takes a string and returns a list of all its rotations.
+
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+   >>> all_rotations = rotations("abaaba$")
+   >>> print(all_rotations)
+   ['abaaba$', 'baaba$a', 'aaba$ab', 'aba$aba', 'ba$abaa', 'a$abaab', '$abaaba']
+  ```
+</details>
+
+**b)** Build the Burrows-Wheeler matrix. Implement the function `bwm` which takes a string and returns the Burrows-Wheeler matrix.
+The matrix should be a list of string rotations sorted alphabetically.
+
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+   >>> matrix = bwm("abaaba$")
+   >>> print(matrix)
+   ['$abaaba', 'a$abaab', 'aaba$ab', 'aba$aba', 'abaaba$', 'ba$abaa', 'baaba$a']
+  ```
+</details>
+
+**с)** Implement the transformation with the Burrows-Wheeler matrix.
+Implement the function `bwt_with_bwm` which takes a string and returns the Burrows-Wheeler transform as a string.
+
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+   >>> t = bwt_with_bwm("abaaba$")
+   >>> print(t)
+   abba$aa
+  ```
+</details>
+
+
+**d)** Check your understandings of the Burrows-Wheeler transform.
+Implement the function `transformation_to_first_colum` which takes the BW transform string t (the last column of the BW matrix) and returns the string which correspond to the first column of the matrix.
+Note that you do not need to build the Burrows-Wheeler matrix to do this.
+
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+   >>> f = transformation_to_first_colum("abba$aa")
+   >>> print(f)
+   $aaaabb
+  ```
+</details>
+
+
+**e)** Implement a helper function `transformation_to_dict_occurrences`.
+This function takes the string transformation (t) and returns the dictionary with the number of occurrences of each character in the transformation.
+
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+   >>> occurrences = transformation_to_dict_occurrences("abba$aa")
+   >>> print(occurrences)
+   {'a': 4, 'b': 2, '$': 1}
+  ```
+</details>
+
+
+**f)** Implement a helper function `transformation_b_ranking`. This function takes the string transformation (t) and returns the B-ranking of the characters in the transformation as a list.
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+   >>> ranks = transformation_b_ranking("abba$aa")
+   >>> print(ranks)
+   >>> print(occurrences)
+   [0, 0, 1, 1, 0, 2, 3]
+   {'a': 4, 'b': 2, '$': 1}
+  ```
+</details>
+
+**g)** Implement the helper function `bwm_first_column_interval_form_from_transform`. This function takes the string transformation (t) and returns
+    the first column of the BW matrix in the interval form
+    i.e. the dictionary where the keys are the characters
+    and the values are tuples (start, end) of the character occurrences. 
+
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+   >>> f_column_intervals = bwm_first_column_interval_form_from_transform("abba$aa")
+   >>> print(f_column_intervals)
+   {'$': (0, 1), 'a': (1, 5), 'b': (5, 7)}
+  ```
+
+</details>
+
+**g)** Implement the function `reverse_bwt`.
+This function takes the BW transformation string (t) and returns the original string.
+
+
+<details>
+  <summary>Example: (Spoiler)</summary>
+
+  ```
+   >>> s = reverse_bwt("abba$aa")
+   >>> print(s)
+   abaaba$
+  ```
+
+</details>
--- helpers/implementation.py
+++ helpers/implementation.py
@@ -41,7 +41,7 @@ def transformation_to_dict_occurrences_correct(t):
     return dict_occurrences
 
 
-def transformation_ranking_occurrences_correct(t):
+def transformation_b_ranking_correct(t):
     """
         This function takes the string transformation (t) and returns the ranking of the transformation.
         """
@@ -52,10 +52,10 @@ def transformation_ranking_occurrences_correct(t):
             dict_occurrences[c] = 0
         ranks.append(dict_occurrences[c])
         dict_occurrences[c] += 1
-    return ranks, dict_occurrences
+    return ranks
 
 
-def bwm_first_column_interval_form_from_transform(t):
+def bwm_first_column_interval_form_from_transform_correct(t):
     """
     This function takes the string transformation (t) and returns
     the first column of the BW matrix in the interval form.
@@ -64,7 +64,7 @@ def bwm_first_column_interval_form_from_transform(t):
     """
     f_colum_cumulative = {}
     c_sum = 0
-    _, dict_occurrences = transformation_ranking_occurrences_correct(t)
+    dict_occurrences = transformation_to_dict_occurrences_correct(t)
     for c, o in sorted(dict_occurrences.items()):
         f_colum_cumulative[c] = c_sum, c_sum + o
         c_sum += o
@@ -75,8 +75,9 @@ def reverse_bwt_correct(t):
     """
     This function takes the BW transformation string (t) and returns the original string.
     """
-    ranks, occurrences = transformation_ranking_occurrences_correct(t)
-    first_column_intervals = bwm_first_column_interval_form_from_transform(t)
+    occurrences = transformation_to_dict_occurrences_correct(t)
+    ranks = transformation_b_ranking_correct(t)
+    first_column_intervals = bwm_first_column_interval_form_from_transform_correct(t)
     row_index = 0
     s = "$"
     while t[row_index] != "$":
@@ -93,20 +94,31 @@ def main():
 
     print("\n_________________2_______________________")
     bwm = bwm_correct(string)
+    print(bwm)
     for r in bwm:
         print(r)
 
     print("\n_________________3_______________________")
+    string_long = 'The word squirrel, first attested in 1327, comes from the Anglo-Norman esquirel which is from the Old French escurel, the reflex of a Latin word sciurus, which was taken from the Ancient Greek word σκίουρος (skiouros; from σκία-ουρος) shadow-tailed, referring to the long bushy tail which many of its members have.The native Old English word for the squirrel, ācweorna, survived only into Middle English (as aquerne) before being replaced.[4] The Old English word is of Common Germanic origin, cognates of which are still used in other Germanic languages, including the German Eichhörnchen (diminutive of Eichhorn, which is not as frequently used), the Norwegian ikorn/ekorn, the Dutch eekhoorn, the Swedish ekorre and the Danish egern. A group of squirrels is called a "dray"[5] or a "scurry".[6] '
+    string_dna = "cctgaatcaagggccaactcatcaagcgggttaacttagaaacagccatgatcagtgaggtcatttacgccctaacccaacggccgcatttccgttcccaacaggaaccaattgacgggtgtactcctctagtatccgagccacttaccccgagacctcacgcgcacgcacgtgatacgagttgcttctatcgaacccattatgaaacaaggactgccttctcagctgtatagtgttctacttagtgagtactcccccttcggtttttatctctcagagccggcatctatgcccccttttgccgtagtacaaattaggaccagcgtgtagctaaagacgtacctgagcaactttgggcgaccttgtggttacgtatgtatagaggcaatttcaagcgtaaacagtgtgcacttctccttcctgttttgaacgctcttcaccgatggaggagggtctttctcaccagcccgtgcacagaccacattgggatacaggagagattaacgcaacgatactggaccgtcgacttccaggtctagttgtttccgggatcctcaaggtctgctaccaagatatcaaccttttaacagcgcacctcggattctagcctgatacaatgaacgccctcctgacccgttcaagtgctaaggcgagttaactcacggctggcacgttaggtggagatagtaatccgtctattagctcctcgtaagaccgcacgattcgggctgaaagatcttatgtaatgtggcggacacagggatactgatcctcgcgcttgtatacgaaatcgacctgcacgagtaacgttggtaaagttaacacttgcttaagacgtccagtgcccgggcgaattaccccaattaaaggacgtgagactcggaagtacgagctggcgatggagtggaaaaagcgacggatatgttgataccgttgcccagaagacatactgcttgggcccactccacgctgcacgttgtagctgttcacagagggaccc"
+
     transform = bwt_with_bwm_correct(string)
+    transform_long = bwt_with_bwm_correct(string_long)
+    transform_dna = bwt_with_bwm_correct(string_dna)
     print(transform)
+    print(transform_long)
+    print(transform_dna)
 
     print("\n________________4_______________________")
     print(transformation_to_first_colum_correct(transform))
 
     print("\n_________________5_______________________")
-    print(transformation_ranking_occurrences_correct(bwt_with_bwm_correct(string)))
+    print(transformation_to_dict_occurrences_correct(bwt_with_bwm_correct(string)))
 
     print("\n_________________6_______________________")
+    print(bwm_first_column_interval_form_from_transform_correct(transform))
+
+    print("\n_________________7_______________________")
     print(reverse_bwt_correct(transform))
 
 
