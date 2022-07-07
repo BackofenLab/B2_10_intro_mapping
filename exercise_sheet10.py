f32b6fcf4f5d6caa493b186bf50aeb8198f0b58d
domonik <schdruzzi@gmail.com>
2022-07-07 10:22:48 +0200

    added skeleton functions

--- exercise_sheet10.py
+++ exercise_sheet10.py
@@ -1,1 +1,33 @@
+from typing import List, Dict, Tuple
 
+
+def rotations(s: str) -> List[str]:
+    return
+
+
+def bwm(s: str) -> List[str]:
+    return
+
+
+def bwt_with_bwm(s: str) -> str:
+    return
+
+
+def transformation_to_first_column(bwt: str) -> str:
+    return
+
+
+def transformation_to_dict_occurrences(bwt: str) -> Dict[str, int]:
+    return
+
+
+def transformation_b_ranking(bwt: str) -> List[int]:
+    return
+
+
+def bwm_first_column_interval_form_from_transform(bwt: str) -> Dict[str, Tuple[int, int]]:
+    return
+
+
+def reverse_bwt(bwt: str) -> str:
+    return
